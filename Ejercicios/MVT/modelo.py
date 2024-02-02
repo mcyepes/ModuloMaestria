048b0b21f2ed74f6bb17e9e783098a40d116dd39
mcyepes <90521594+mcyepes@users.noreply.github.com>
2024-02-02 23:52:49 +0000

    Initial commit

---
+++ Ejercicios/MVT/modelo.py
@@ -0,0 +1,15 @@
+def obtener_libros():
+  #Simulacion BD
+  return [{
+      "id": 1,
+      "titulo": "Cien años de soledad",
+      "autor": "Gabriel Garcia"
+  }, {
+      "id": 2,
+      "titulo": "La sombra del viento",
+      "autor": "Carlos Ruiz"
+  }, {
+      "id": 3,
+      "titulo": "El nombre del viento",
+      "autor": "Patrick Rothfuss"
+  }]
